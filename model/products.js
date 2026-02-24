794695a17d03d6c21f53bbeeb0927896710321f0
C:/Users/USER/car rental proj/.git/ <obenagyamfi17@gmail.com>
2026-02-24 16:05:26 +0000

    initial commit

---
+++ model/products.js
@@ -0,0 +1,79 @@
+const products =[
+    {id, name:'', price, category, stock, imageUrl},
+     {id, name:'', price, category, stock, imageUrl},
+      {id, name:'', price, category, stock, imageUrl},
+       {id, name:'', price, category, stock, imageUrl}
+];
+
+const electronics=[
+     {id, name:'', price, category, stock, imageUrl},
+      {id, name:'', price, category, stock, imageUrl},
+       {id, name:'', price, category, stock, imageUrl},
+        {id, name:'', price, category, stock, imageUrl},
+         {id, name:'', price, category, stock, imageUrl}
+];
+
+const clothing=[
+     {id, name:'', price, category, stock, imageUrl},
+      {id, name:'', price, category, stock, imageUrl},
+       {id, name:'', price, category, stock, imageUrl},
+        {id, name:'', price, category, stock, imageUrl},
+         {id, name:'', price, category, stock, imageUrl}
+];
+
+const books=[
+    {id, name:'', price, category, stock, imageUrl},
+      {id, name:'', price, category, stock, imageUrl},
+       {id, name:'', price, category, stock, imageUrl},
+        {id, name:'', price, category, stock, imageUrl},
+         {id, name:'', price, category, stock, imageUrl}
+];
+
+const sports=[
+    {id, name:'', price, category, stock, imageUrl},
+      {id, name:'', price, category, stock, imageUrl},
+       {id, name:'', price, category, stock, imageUrl},
+        {id, name:'', price, category, stock, imageUrl},
+         {id, name:'', price, category, stock, imageUrl}
+];
+
+const beauty =[
+    {id, name:'', price, category, stock, imageUrl},
+      {id, name:'', price, category, stock, imageUrl},
+       {id, name:'', price, category, stock, imageUrl},
+        {id, name:'', price, category, stock, imageUrl},
+         {id, name:'', price, category, stock, imageUrl}
+];
+
+const getAllProducts =()=>{
+    return products;
+};
+
+const getAllElectronics =()=>{
+    return electronics;
+};
+
+const getAllClothing =()=>{
+    return clothing;
+};
+
+const getAllBooks =()=>{
+    return books;
+};
+
+const getAllSports =()=>{
+    return sports;
+};
+
+const getAllBeauty =()=>{
+    return beauty;
+};
+
+module.exports ={
+    getAllProducts,
+    getAllElectronics,
+    getAllClothing,
+    getAllBooks,
+    getAllSports,
+    getAllBeauty
+};
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+
+const server = http.createServer(app);
+
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+});
